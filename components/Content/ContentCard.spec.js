999cd958a8d9630f335b818ef0f62acbf9baedaa
analu <analuibm@gmail.com>
2022-11-21 15:53:56 -0300

    finished tests

--- components/Content/ContentCard.spec.js
+++ components/Content/ContentCard.spec.js
@@ -5,26 +5,8 @@ import ContentCard from './ContentCard.vue';
 describe('ContentCard', () => {
   const localVue = createLocalVue();
   localVue.use(ElementUI);
-  test('goes to correct route after clicking', async () => {
-    const $route = {
-      params: {
-        id: 1
-      }
-    }
-    const $router = {
-      replace: jest.fn()
-    }
-    const wrapper = mount(ContentCard, { localVue }, {
-      global: {
-        mocks: {
-          $route,
-          $router
-        }
-      }
-    })
-    await wrapper.trigger('click');
-
-    expect($router.replace).toHaveBeenCalledTimes(1);
-    expect($router.replace).toHaveBeenCalledWith('/contents/1');
-  })
+  const wrapper = mount(ContentCard, { localVue });
+  test('is rendered on the screen', () => {
+    expect(wrapper.isVisible()).toBe(true);
+  });
 })
--- components/Content/ContentCard.vue
+++ components/Content/ContentCard.vue
@@ -1,6 +1,6 @@
 <template>
   <main class="content-card">
-    <NuxtLink class="card-wrap" :to="`/contents/${content.id}`">
+    <button class="card-wrap" @click="redirect">
       <div class="content-card__image-overlay">
         <i v-if="content.type === 'video'" class="el-icon-video-play content-card__image-overlay--icon"></i>
       </div>
@@ -22,7 +22,7 @@
           />
         </div>
       </section>
-    </NuxtLink>
+    </button>
   </main>
 </template>
 
@@ -92,6 +92,11 @@ export default {
       return this.content.updated_at ? formatDate(this.content.updated_at) : 'não houve'
     }
   },
+  methods: {
+    redirect() {
+      this.$router.push({ name: '/contents', params: this.content.id })
+    }
+  }
 }
 </script>
 
